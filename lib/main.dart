d2ebfd5cb344788d2415182fb549c533e9ecd147
Leonardo Carvalho <leo.carvalho100@gmail.com>
2020-10-23 21:19:57 -0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import "package:flutter/material.dart";
+import "package:buscador_gifs/ui/home_page.dart";
+
+import 'package:buscador_gifs/ui/gif_page.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: new HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
+
+
